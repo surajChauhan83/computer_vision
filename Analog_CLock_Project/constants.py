117f99d6f3163e1c84c3b959ba0ab746458f5988
SURAJ CHOUHAN <84663882+surajChauhan83@users.noreply.github.com>
2024-07-16 07:15:59 +0530

    Add files via upload

---
+++ Analog_CLock_Project/constants.py
@@ -0,0 +1,11 @@
+#define color for clock makeup
+#here we are making dictionary of colors so hat we may access them whenever needded
+
+COLORS = {'blue': (255,0,0), 'green':(0,255,0), 'red':(0,0,255), 'yellow':(0,255,255), 'magenta':(255,0,255), 'cyan':(255,255,0),
+          'white':(255,255,255), 'amber':(255,191,0), 'gray':(125,125,125), 'dark_gray':(50,50,50), 'light_gray':(220,220,220),
+          'black':(0,0,0)}
+
+#radius of the clock
+RADIUS = 260
+CENTER = (320,320)
+CANVAS_SIZE = (640,640,3)
---
+++ Analog_CLock_Project/helperFunction.py
@@ -0,0 +1,44 @@
+import cv2
+import datetime
+import math
+from constants import COLORS
+from constants import RADIUS
+from constants import CENTER
+
+def get_ticks():
+    hours_init = []
+    hours_det = []
+
+    for i in range(0,360,6):
+        x_coordinate =int(CENTER[0] + RADIUS * math.cos(i * math.pi /180))
+        y_coordinate = int(CENTER[1] + RADIUS * math.sin(i * math.pi /180))
+
+        hours_init.append((x_coordinate,y_coordinate))
+
+    for i in range(0,36,6):
+        x_coordinate = int(CENTER[0] + (RADIUS-20) * math.cos(i * math.pi/180))
+        y_coordinate = int(CENTER[1] + (RADIUS-20) * math.sin(i * math.pi/180))
+
+        hours_det.append((x_coordinate,y_coordinate))
+
+    return hours_init, hours_det
+
+def getDigitalTime(h,m,s):
+    time = ""
+    hour = ""
+    minute = ""
+    second = ""
+    if(h<10):
+        hour = "0{}:".format(h)
+    else:
+        hour = "{}:".format(h)
+    if(m<10):
+        minute = "0{}:".format(m)
+    else:
+        minute = "{}:".format(m)
+    if(s<10):
+        second = "0{}:".format(s)
+    else:
+        second = "{}:".format(s)
+    time = hour+minute+second
+    return time
